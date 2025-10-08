d9fbd6aa2809e0c1416a0bf01117b9759ca15a1e
Anindo Roy <anindoroy112@gmail.com>
2025-10-08 21:14:18 +0600

    Revert "Reapply "update""
    This reverts commit 3fd52c370a553298a97373774c3fb0f7efdbf918.

--- app/shop/page.tsx
+++
@@ -1,6 +0,0 @@
-import ProductGrid from "@/components/ProductGrid";
-import { products } from "@/data/products";
-
-export default function ShopPage() {
-  return <ProductGrid items={products} title="All Products" />;
-}
