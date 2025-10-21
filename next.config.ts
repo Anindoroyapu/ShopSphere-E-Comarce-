618891bef1a2c41ceab78c26d7ee009687e4e11b
farabirakib <mdrikib399@gmail.com>
2025-10-21 12:44:05 +0600

    update6

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,44 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     domains: ["images.unsplash.com"],
+//   },
+// };
+
+// export default nextConfig;
+
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com", // Google avatars
+      },
+      {
+        protocol: "https",
+        hostname: "i.pravatar.cc", // fallback avatar
+      },
+      {
+        protocol: "http",
+        hostname: "127.0.0.1", // local storage
+      },
+      {
+        protocol: "http",
+        hostname: "localhost", // local storage (alternative)
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com", // Unsplash images
+      },
+    ],
   },
 };
 
 export default nextConfig;
+
+
+
